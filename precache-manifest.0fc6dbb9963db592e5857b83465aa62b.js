af129177b743377a8cfcee4c52e728247523e849
RichardLiao <bnmghjtyu0@gmail.com>
2020-08-20 16:57:45 +0800

    Updates

---
+++ precache-manifest.0fc6dbb9963db592e5857b83465aa62b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea881ee9c62e44598773e8bd0160852",
+    "url": "/richtext-draft/index.html"
+  },
+  {
+    "revision": "31d0d32c525cfdf229e1",
+    "url": "/richtext-draft/static/css/main.03b1281a.chunk.css"
+  },
+  {
+    "revision": "f79100cb876cc15287c1",
+    "url": "/richtext-draft/static/js/2.2a3daf18.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/richtext-draft/static/js/2.2a3daf18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d0d32c525cfdf229e1",
+    "url": "/richtext-draft/static/js/main.d002354e.chunk.js"
+  },
+  {
+    "revision": "ab7c4c2eda1d0462344f",
+    "url": "/richtext-draft/static/js/runtime-main.72f9518b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/richtext-draft/static/media/logo.5d5d9eef.svg"
+  }
+]);
